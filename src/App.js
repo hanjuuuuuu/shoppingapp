682f49d6d3dfadc1a6f8d1f9e5918df55df8e9ca
hanjuuuuuu <hanjucomss@naver.com>
2022-07-29 17:31:47 +0900

    라디오버튼 직접입력으로 변경

--- src/App.js
+++ src/App.js
@@ -5,6 +5,24 @@ import React from 'react';
 import 'antd/dist/antd.min.css';
 
 const { Option } = Select;
+const moment = require('moment')
+
+let date = new Date();    //날짜 초기값 설정
+let year = String(date.getFullYear());
+let tmonth = date.getMonth();
+tmonth += 1;
+if(tmonth <= 9){   //두자리 수로 반환한다.
+  tmonth = "0" + tmonth;
+}
+let fmonth = tmonth - 1
+if(fmonth <= 9){   
+  fmonth = "0" + fmonth;
+}
+let day = date.getDate();
+day -= 1;
+if (day <= 9){
+  day = "0" + day;
+}
 
 const App = () => {
   const [form] = Form.useForm();
@@ -23,48 +41,59 @@ const App = () => {
   
     }
 
-  const getDay = () => {  //날짜 구하기(yyyy-mm-dd형식)
-    var date = new Date();
-    var year = date.getFullYear();
-    var month = ("0" + (date.getMonth() + 1).slice(-2));  //문자열로 만들어서 항상 2자리로 반환하게 함
-    var day = ("0" + (date.getDate()).slice(-2));
-
-    return year + "-" + month + "-" + day;
-  }
-
-  const onClick = () => {
-    var fieldData = form.getFieldsValue()
+  const getDay = (changeDate) => {  //날짜를 (yyyy-mm-dd형식)으로 만들기
+    let datetemp = moment().subtract(1,'days').calendar().format("YYYYMMDD");
+    const fielddate = form.getFieldsValue()
+    fielddate.dateFromYear = datetemp.substring(0,4);
+    fielddate.dateFromMonth = changeDate.substring(4,6);
+    fielddate.dateFromDay = changeDate.substring(6,8);
     
-    queryTemplete.startDate = fieldData
-    queryTemplete.endDate =
-    queryTemplete.timeUnit = fieldData.timeUnit
-    queryTemplete.category.name = 
-    queryTemplete.category.param = 
-    queryTemplete.device = fieldData.device
-    queryTemplete.gender = fieldData.gender
-    queryTemplete.ages.push(fieldData.ages)
-
-
+     
   }
 
-  const onChange = (timerange) => {   //라디오버튼 눌렸을 때 해당하는 날짜로 변경(1개월, 3개월, 1년)
+  const onChange = (timerange) => {   //라디오버튼 눌렀을 때 해당하는 날짜로 변경(1개월, 3개월, 1년)
+    let dateset = moment().subtract(1,'days').calendar().format("YYYYMMDD")
+    let fieldData = form.getFieldsValue()
     if(timerange = "1m")  
-      fieldData.startDate = get
-    if(timerange = "3m")
-
-    if(timerange = "1y")
+      dateset = dateset.moment().subtract(1,'months').calendar().format("YYYYMMDD")  
+    else if(timerange = "3m")
+      dateset= dateset.moment().subtract(3,'months').calendar().format("YYYYMMDD")
+    else if(timerange = "1y")
+      dateset = dateset.moment().subtract(1, 'years').calendar().format("YYYYMMDD") 
   }
+
+  const selfChange = () => {      //날짜 직접 바꾸면 라디오버튼을 직접입력으로 변경 
+    let rbutton = form.getFieldsValue()
+    rbutton.daterange = "self"
+    form.setFieldsValue(rbutton)
+  }   
+
+    const onClick = () => {     //조회하기 버튼 클릭하면 서버에 정보 전송하기
+    // let fieldData = form.getFieldsValue()
+    
+    // queryTemplete.startDate = fieldData
+    // queryTemplete.endDate =
+    // queryTemplete.timeUnit = fieldData.timeUnit
+    // queryTemplete.category.name = 
+    // queryTemplete.category.param = 
+    // queryTemplete.device = fieldData.device
+    // queryTemplete.gender = fieldData.gender
+    // queryTemplete.ages.push(fieldData.ages)
+  }   
 
   
   return (
     <>
+    <Form
+      form={form}
+    >
     <dir>  분야 통계 </dir>
     <br />
     <br />
     <Row>
-    <Typography.Text> 분야 </Typography.Text>
+    <Typography.Text strong style={{marginLeft:10}}> 분야 </Typography.Text>
     <Form.Item name="category">
-    <Select defaultValue="패션의류" style={{ width: 200, marginTop: 30, marginRight: 30}}>
+    <Select defaultValue="패션의류" style={{width: 200, marginTop: 30, marginRight: 30}}>
       <Option value="패션의류">패션의류</Option>
       <Option value="패션잡화">패션잡화</Option>
       <Option value="화장품/미용">화장품/미용</Option>
@@ -82,26 +111,26 @@ const App = () => {
     </Row>
     
     <Row>
-    <Typography.Text> 기간 </Typography.Text>
+    <Typography.Text strong style={{marginLeft:10}}> 기간 </Typography.Text>
     <Form.Item name="timeUnit">
-    <Select defaultValue="date" style={{ width: 100, marginTop: 30, marginRight: 30}}>
+    <Select defaultValue="date" style={{width: 100, marginTop: 30, marginRight: 30}}>
       <Option value="date">일간</Option>
       <Option value="week">주간</Option>
       <Option value="month">월간</Option>
     </Select>
     </Form.Item> 
 
-    <Form.Item name="date">
-    <Radio.Group style={{ marginTop: 30, marginRight: 30}} defaultValue="1m">
-      <Radio.Button value="1m" onChange={onChange(1m)}>1개월</Radio.Button>
-      <Radio.Button value="3m">3개월</Radio.Button>
-      <Radio.Button value="1y">1년</Radio.Button>
-      <Radio.Button value="self">직접입력</Radio.Button>
+    <Form.Item name="daterange">
+    <Radio.Group defaultValue="1m" buttonStyle="solid" style={{ marginTop: 30, marginRight: 30}}>
+      <Radio.Button value="1m" onChange={()=>onChange("1m")}>1개월</Radio.Button>
+      <Radio.Button value="3m" onChange={()=>onChange("3m")}>3개월</Radio.Button>
+      <Radio.Button value="1y" onChange={()=>onChange("1y")}>1년</Radio.Button>
+      <Radio.Button value="self" >직접입력</Radio.Button>
     </Radio.Group>
     </Form.Item>
 
     <Form.Item name = "dateFromYear">
-    <Select defaultValue={(getDay.year)} style={{  marginTop: 30, width: 100 }}>
+    <Select defaultValue={year} style={{ marginTop: 30, width: 100 }} onChange={selfChange}>
       <Option value="2017">2017</Option>
       <Option value="2018">2018</Option>
       <Option value="2019">2019</Option>
@@ -111,7 +140,7 @@ const App = () => {
     </Select>
     </Form.Item>
     <Form.Item name = "dateFromMonth">
-    <Select defaultValue={(getDay.month)}  style={{  marginTop: 30, width: 100 }}>
+    <Select defaultValue={fmonth} style={{ marginTop: 30, width: 100 }} onChange={selfChange}>
       <Option value="01">01</Option>
       <Option value="02">02</Option>
       <Option value="03">03</Option>
@@ -127,7 +156,7 @@ const App = () => {
     </Select>
     </Form.Item>
     <Form.Item name = "dateFromDay">
-    <Select defaultValue={getDay.day}   style={{  marginTop: 30, marginRight: 10, width: 100 }}>
+    <Select defaultValue={day} style={{ marginTop: 30, marginRight: 10, width: 100 }} onChange={selfChange}>
       <Option value="01">01</Option>
       <Option value="02">02</Option>
       <Option value="03">03</Option>
@@ -163,7 +192,7 @@ const App = () => {
     </Form.Item>
     <Typography.Text style={{ marginTop: 30}}>  ~  </Typography.Text>
     <Form.Item name = "dateToYear">
-    <Select defaultValue={getDay.year} style={{  marginTop: 30, marginLeft:10, width: 100 }}>
+    <Select defaultValue={year} style={{ marginTop: 30, marginLeft:10, width: 100 }} onChange={selfChange}>
       <Option value="2017">2017</Option>
       <Option value="2018">2018</Option>
       <Option value="2019">2019</Option>
@@ -173,7 +202,7 @@ const App = () => {
     </Select>
     </Form.Item>
     <Form.Item name = "dateToMonth">
-    <Select defaultValue={getDay.month}  style={{ marginTop: 30, width: 100 }}>
+    <Select defaultValue={tmonth} style={{ marginTop: 30, width: 100 }} onChange={selfChange}>
       <Option value="01">01</Option>
       <Option value="02">02</Option>
       <Option value="03">03</Option>
@@ -189,7 +218,7 @@ const App = () => {
     </Select>
     </Form.Item>
     <Form.Item name = "dateToDay">
-    <Select defaultValue={getDay.day} style={{  marginTop: 30, width: 100 }}>
+    <Select defaultValue={day} style={{ marginTop: 30, width: 100 }} onChange={selfChange}>
       <Option value="01">01</Option>
       <Option value="02">02</Option>
       <Option value="03">03</Option>
@@ -222,12 +251,11 @@ const App = () => {
       <Option value="30">30</Option>
       <Option value="31">31</Option>
     </Select>
-
     </Form.Item>
     </Row>
     
     <Row>
-      <Typography.Text>기기별</Typography.Text>
+      <Typography.Text strong style={{marginLeft:10}}>기기별</Typography.Text>
         <Form.Item name="device"> 
           <Checkbox.Group style={{width: '100%', marginLeft: 10}}>
             <Checkbox value="">전체</Checkbox>
@@ -235,8 +263,8 @@ const App = () => {
             <Checkbox value="mo">모바일</Checkbox>
           </Checkbox.Group> 
         </Form.Item>
-        <Typography.Text style={{ marginLeft:15, marginRight: 15}}>|</Typography.Text>
-      <Typography.Text>성별</Typography.Text>
+        <Typography.Text style={{marginLeft:15, marginRight: 15}}>|</Typography.Text>
+      <Typography.Text strong>성별</Typography.Text>
         <Form.Item name="gender"> 
           <Checkbox.Group style={{width: '100%', marginLeft: 10}}>
             <Checkbox value="">전체</Checkbox>
@@ -244,8 +272,8 @@ const App = () => {
             <Checkbox value="m">남성</Checkbox>
           </Checkbox.Group> 
         </Form.Item>
-        <Typography.Text style={{ marginLeft:15, marginRight: 15}}>|</Typography.Text>
-      <Typography.Text>연령</Typography.Text>
+        <Typography.Text style={{marginLeft:15, marginRight: 15}}>|</Typography.Text>
+      <Typography.Text strong>연령</Typography.Text>
         <Form.Item name="ages"> 
           <Checkbox.Group style={{width: '100%', marginLeft: 10}}>
             <Checkbox value="">전체</Checkbox>
@@ -260,13 +288,13 @@ const App = () => {
     </Row>
     <br />
     <br />
-    <Button type="primary" icon={<SearchOutlined />} style={{ marginLeft: 600}} onClick = {onClick}>
+    <Button type="primary" icon={<SearchOutlined />} style={{marginLeft: 600}} onClick = {onClick}>
        조회하기
     </Button>
-    
+   
     <br/>
-    <Line {...chartInfo}/>
 
+    </Form> 
     </>
   );
 }
